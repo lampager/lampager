f7e6bd6375e2a957f2b80e6a3b493767d176d71e
Chitoku <odango@chitoku.jp>
2017-10-29 02:17:34 +0900

    Fix #10: Create Lampager\Contracts\Cursor::has(...$columns)

--- src/Contracts/Cursor.php
+++ src/Contracts/Cursor.php
@@ -8,12 +8,11 @@ namespace Lampager\Contracts;
 interface Cursor
 {
     /**
-     * Return a value indicating whether the cursor has the column.
+     * Return a value indicating whether the cursor is non-empty and has the specified columns.
      *
-     * @param  string $column Column.
-     * @return bool
+     * @return null|bool null if the cursor is empty; true if the cursor has all the columns; otherwise false.
      */
-    public function has($column);
+    public function has(...$columns);
 
     /**
      * Return a cursor specified by the column.
